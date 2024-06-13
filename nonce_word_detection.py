ae5d11ad3c8da7627e2e4048e0b4835aa817354f
Kalvin Chang <kalvin1204@gmail.com>
2024-06-13 03:07:59 -0400

    feat: prompts for nonce word detection

---
+++ nonce_word_detection.py
@@ -0,0 +1,23 @@
+instructions = [
+    "Given these two utterances (utterance 1 and utterance 2), which is not an actual word in the English language? Respond with 1 or 2."
+    "Based on the two audio files (1 and 2), spot which word is not a word in English. The answer should be 1 or 2.",
+    "Please determine whether word 1 or word 2 is not an English word given the two audio files. Respond with 1 or 2.",
+    "Determine if word 1 or word 2 is a fake English word given the two audio files (1 and 2). Write your answer as either 1 or 2.",
+    "Examine if word 1 or word 2 is not an English word given the two audio files (1 and 2)? Write your answer as either 1 or 2.",
+    "Listen to the two audio clips (1 and 2) and judge which of the two is not an English word. Write your answer as either 1 or 2.",
+    "Decide which of the two words is not an actual English word in these two audio clips (1 and 2). The answer could be 1 or 2.",
+    "From the two audio inputs (1 and 2), ascertain if word 1 or word 2 is a fake English word. The answer is either 1 or 2.",
+    "Judge whether word 1 or word 2 is not a word in the English language. Respond with 1 or 2.",
+    "Please determine if word 1 or word 2 is not part of the English lexicon. The answer is either 1 or 2.",
+    "Decide whether word 1 or word 2 is the fake English word in the provided audio clips (1 and 2). The answer is 1 or 2.",
+    "Based on these two audio clips (1 and 2), judge whether word 1 or word 2 is not a word in English. Write your answer as 1 or 2.",
+    "Given the two audio files (1 and 2), determine whether word 1 or word 2 lacks. The answer could be 1 or 2",
+    "Using the two audio files (1 and 2), determine whether word 1 is the fake word or if word 2 is. The answer should be 1 or 2.",
+    "Please identify the fake English word among the two given clips (1 and 2). The answer is either 1 or 2.",
+    "Assess which of the two words is a fake English word, based on the two audio files (1 and 2). Respond with 1 or 2.",
+    "There is a fake word among us. Listen to the two audio files (1 and 2) and decide if word 1 or word 2 is a fake English word. Your answer should be 1 or 2.",
+    "Using the two audio recordings (1 and 2), judge whether word 1 or word 2 is not an English word. Your answer should be either 1 or 2.",
+    "From the two audio clips (1 and 2), decide whether word 1 or word 2 is a fake English word. Write 1 or 2 as your answer.",
+    "Using the two audio recordings (1 and 2), decide if word 1 or word 2 is not a real English word. Your answer should be either 1 or 2.",
+    "With the provided audio files (1 and 2), decide which of the two words is a fake English word. Indicate your answer as either 1 or 2."
+]
