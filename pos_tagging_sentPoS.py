8fcb008b133297864dba15482060c1a96858231f
EunJung Yeo <eyeo2@babel-login-3.lti.cs.cmu.edu>
2024-06-27 08:09:16 -0400

    update instructions

--- pos_tagging_sentPoS.py
+++ pos_tagging_sentPoS.py
@@ -9,19 +9,39 @@ from utils import validate_dataset
 stanza.download('en')
 nlp = stanza.Pipeline('en', use_gpu=False)
 
+
 tag_description = "The tags include ADJ, ADP, ADV, AUX, CCONJ, DET, INTJ, NOUN, NUM, PART, PRON, PROPN, SCONJ, and VERB."
-instructions = [
-    "For each word in the given audio utterance, determine and assign the corresponding Part-of-Speech (POS) tag. " + tag_description + " Write the transcribed utterance first, and then the POS tags followed by a '/'. For example, for 'The quick brown fox jumps over the lazy dog', generate the tags in the form of (transcription): The quick brown fox jumps over the lazy dog / (POS): DET ADJ ADJ NOUN VERB ADP DET ADJ NOUN.",
-    "For each word in the given audio clip, identify and assign the appropriate Part-of-Speech (POS) tag. " + tag_description + " Write the transcription of the utterance first, followed by the POS tags separated by a '/'. For example, for 'She decided to take a walk in the beautiful park despite the rain', format the response as (transcription): She decided to take a walk in the beautiful park despite the rain / (POS): PRON VERB PART VERB DET NOUN ADP DET ADJ NOUN SCONJ DET NOUN.",
-    "Listen to the audio and determine the Part-of-Speech (POS) tag for each word in the utterance. " + tag_description + " Write the transcription first, then the POS tags separated by a '/'. For example, for 'He was excited to receive the prestigious award at the annual ceremony', write (transcription): He was excited to receive the prestigious award at the annual ceremony / (POS): PRON AUX ADJ PART VERB DET ADJ NOUN ADP DET ADJ NOUN.",
-    "For each word in the audio, identify its Part-of-Speech (POS) tag. " + tag_description + " Transcribe the utterance first, followed by the POS tags separated by a '/'. For instance, 'Although it was raining, they decided to go for a hike in the mountains' should be formatted as (transcription): Although it was raining, they decided to go for a hike in the mountains / (POS): SCONJ PRON AUX VERB PRON VERB PART VERB ADP DET NOUN ADP DET NOUN.",
-    "After listening to the audio, assign the correct Part-of-Speech (POS) tag to each word. " + tag_description + " Write the transcription first, then the POS tags separated by a '/'. For example, for 'The children enjoyed playing outside even though it was cold and windy', write (transcription): The children enjoyed playing outside even though it was cold and windy / (POS): DET NOUN VERB VERB ADV SCONJ PRON AUX ADJ CCONJ ADJ.",
-    "Determine the Part-of-Speech (POS) tag for each word in the audio utterance. " + tag_description + " Transcribe the utterance first, followed by the POS tags separated by a '/'. For example, for 'She was delighted to see her friends after such a long time', write (transcription): She was delighted to see her friends after such a long time / (POS): PRON AUX ADJ PART VERB PRON NOUN ADP DET ADJ NOUN.",
-    "Identify and assign the Part-of-Speech (POS) tag for each word in the given audio. " + tag_description + " Write the transcription first, then the POS tags separated by a '/'. For instance, for 'They didn't realize how much they had missed the city until they returned', write (transcription): They didn't realize how much they had missed the city until they returned / (POS): PRON AUX PART VERB ADV ADV PRON AUX VERB DET NOUN SCONJ PRON VERB.",
-    "Listen to the audio and determine the Part-of-Speech (POS) tag for each word. " + tag_description + " Write the transcription first, then the POS tags separated by a '/'. For example, for 'Despite the long journey, they were happy to reach their destination', write (transcription): Despite the long journey, they were happy to reach their destination / (POS): ADP DET ADJ NOUN PRON AUX ADJ PART VERB PRON NOUN.",
-    "For each word in the audio clip, identify its Part-of-Speech (POS) tag. " + tag_description + " Write the transcription first, followed by the POS tags separated by a '/'. For instance, 'The conference was postponed due to unforeseen circumstances', should be formatted as (transcription): The conference was postponed due to unforeseen circumstances / (POS): DET NOUN AUX VERB ADP ADJ NOUN.",
-    "Determine and assign the Part-of-Speech (POS) tag for each word in the given audio. " + tag_description + " Write the transcription first, then the POS tags separated by a '/'. For example, for 'The new policy will affect all employees starting next month', write (transcription): The new policy will affect all employees starting next month / (POS): DET ADJ NOUN AUX VERB DET NOUN VERB ADP NOUN."
+
+# First part of the new set of instructions
+first = [
+    "For each word in the given audio utterance, determine and assign the corresponding Part-of-Speech (POS) tag.",
+    "For each word in the given audio clip, identify and assign the appropriate Part-of-Speech (POS) tag.",
+    "Listen to the audio and determine the Part-of-Speech (POS) tag for each word in the utterance.",
+    "For each word in the audio, identify its Part-of-Speech (POS) tag.",
+    "After listening to the audio, assign the correct Part-of-Speech (POS) tag to each word.",
+    "Determine the Part-of-Speech (POS) tag for each word in the audio utterance.",
+    "Identify and assign the Part-of-Speech (POS) tag for each word in the given audio.",
+    "Listen to the audio and determine the Part-of-Speech (POS) tag for each word.",
+    "For each word in the audio clip, identify its Part-of-Speech (POS) tag.",
+    "Determine and assign the Part-of-Speech (POS) tag for each word in the given audio."
 ]
+
+# Second part of the new set of instructions with examples
+second = [
+    "Write the transcribed utterance first, and then the POS tags followed by a '/'. For example, for 'The quick brown fox jumps over the lazy dog', generate the tags in the form of: The quick brown fox jumps over the lazy dog / DET ADJ ADJ NOUN VERB ADP DET ADJ NOUN.",
+    "Write the transcription of the utterance first, followed by the POS tags separated by a '/'. For example, for 'She decided to take a walk in the beautiful park despite the rain', format the response as: She decided to take a walk in the beautiful park despite the rain / PRON VERB PART VERB DET NOUN ADP DET ADJ NOUN SCONJ DET NOUN.",
+    "Write the transcription first, then the POS tags separated by a '/'. For example, for 'He was excited to receive the prestigious award at the annual ceremony', write: He was excited to receive the prestigious award at the annual ceremony / PRON AUX ADJ PART VERB DET ADJ NOUN ADP DET ADJ NOUN.",
+    "Transcribe the utterance first, followed by the POS tags separated by a '/'. For instance, 'Although it was raining, they decided to go for a hike in the mountains' should be formatted as: Although it was raining, they decided to go for a hike in the mountains / SCONJ PRON AUX VERB PRON VERB PART VERB ADP DET NOUN ADP DET NOUN.",
+    "Write the transcription first, then the POS tags separated by a '/'. For example, for 'The children enjoyed playing outside even though it was cold and windy', write: The children enjoyed playing outside even though it was cold and windy / DET NOUN VERB VERB ADV SCONJ PRON AUX ADJ CCONJ ADJ.",
+    "Transcribe the utterance first, followed by the POS tags separated by a '/'. For example, for 'She was delighted to see her friends after such a long time', write: She was delighted to see her friends after such a long time / PRON AUX ADJ PART VERB PRON NOUN ADP DET ADJ NOUN.",
+    "Write the transcription first, then the POS tags separated by a '/'. For instance, for 'They didn't realize how much they had missed the city until they returned', write: They didn't realize how much they had missed the city until they returned / PRON AUX PART VERB ADV ADV PRON AUX VERB DET NOUN SCONJ PRON VERB.",
+    "Write the transcription first, then the POS tags separated by a '/'. For example, for 'Despite the long journey, they were happy to reach their destination', write: Despite the long journey, they were happy to reach their destination / ADP DET ADJ NOUN PRON AUX ADJ PART VERB PRON NOUN.",
+    "Write the transcription first, followed by the POS tags separated by a '/'. For instance, 'The conference was postponed due to unforeseen circumstances', should be formatted as: The conference was postponed due to unforeseen circumstances / DET NOUN AUX VERB ADP ADJ NOUN.",
+    "Write the transcription first, then the POS tags separated by a '/'. For example, for 'The new policy will affect all employees starting next month', write: The new policy will affect all employees starting next month / DET ADJ NOUN AUX VERB DET NOUN VERB ADP NOUN."
+]
+
+# Using list comprehension to generate the combined list
+instructions = [f + " " + tag_description + " " + s for f in first for s in second]
 
 def filter_text(text):
     unwanted_chars = set("\"`;-:")
